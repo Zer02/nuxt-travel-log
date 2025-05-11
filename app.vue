8538b188bfe2061be6e4e3f18aa1960d07a34cf4
Zer02 <abasdeo29@gmail.com>
2025-05-10 20:51:05 -0400

    added landing page

--- app.vue
+++ app.vue
@@ -1,10 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <button class="btn btn-soft btn-secondary">
-      View Map <Icon name="tabler:map" size="29" />
-    </button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
